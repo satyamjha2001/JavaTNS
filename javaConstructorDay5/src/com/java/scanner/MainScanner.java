1ff659cf26578baf7f127e71eb68567604944984
Satyam Kumar Jha <satyamjha23112001@gmail.com>
2024-01-30 23:15:57 +0530

    commit day 5

---
+++ javaConstructorDay5/src/com/java/scanner/MainScanner.java
@@ -0,0 +1,10 @@
+package com.java.scanner;
+import com.java.scanner.utility.ScannerUtility;
+public class MainScanner {
+
+	public static void main(String[] args) {
+		ScannerUtility utility = new ScannerUtility();
+//		utility.acceptNumbers();
+		utility.acceptCharacters();
+	}
+}
---
+++ javaConstructorDay5/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module javaConstructorDay5 {
+}
